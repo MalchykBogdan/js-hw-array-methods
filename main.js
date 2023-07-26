6de94e5f6706918609e234a965be5dec1da5864c
MalchykBogdan <124812110+MalchykBogdan@users.noreply.github.com>
2023-07-26 16:44:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,154 @@
+let users =  [
+{
+    id: '701b29c3-b35d-4cf1-a5f6-8b12b29a5081',
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    skills: ['ipsum', 'lorem'],
+    gender: 'male',
+    age: 37,
+  },
+  {
+    id: '7a3cbd18-57a1-4534-8e12-1caad921bda1',
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
+    gender: 'female',
+    age: 34,
+  },
+  {
+    id: '88beb2f3-e4c2-49f3-a0a0-ecf957a95af3',
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
+    gender: 'male',
+    age: 24,
+  },
+  {
+    id: '249b6175-5c30-44c6-b154-f120923736f5',
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    skills: ['adipisicing', 'irure', 'velit'],
+    gender: 'female',
+    age: 21,
+  },
+  {
+    id: '334f8cb3-eb04-45e6-abf4-4935dd439b70',
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    skills: ['ex', 'culpa', 'nostrud'],
+    gender: 'male',
+    age: 27,
+  },
+  {
+    id: '150b00fb-dd82-427d-9faf-2879ea87c695',
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    skills: ['non', 'amet', 'ipsum'],
+    gender: 'male',
+    age: 38,
+  },
+  {
+    id: 'e1bf46ab-7168-491e-925e-f01e21394812',
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    skills: ['lorem', 'veniam', 'culpa'],
+    gender: 'female',
+    age: 39,
+  },
+];
+
+// Завдання перше
+// users.forEach(getUserNames =>{
+//     console.log(getUserNames.name);
+// });
+const getUserNames = users => {
+    return users.map(user => user.name)
+};
+
+console.log(getUserNames(users));
+
+// Завданя друге
+const getUsersWithEyeColor = (users, color) => {
+    return users.filter(user => user.eyeColor === color);
+};
+
+const colorToFind = 'blue';
+const findUsersEyeColor = getUsersWithEyeColor(users,colorToFind )
+
+console.log(findUsersEyeColor);
+
+//Завдання третє
+const getUsersWithGender = (users, gender) => {
+    return users
+        .filter(user => user.gender === gender)
+        .map(user => user.name);
+};
+
+const filterForGander = 'female'
+const findNameForGander = getUsersWithGender(users, filterForGander)
+
+console.log(findNameForGander);
+
+//Завдання четверте 
+const getInactiveUsers = users => {
+    return users.filter(user => !user.isActive).map(user => user.name)
+};
+
+const active = "true"
+const userIsActive = getInactiveUsers(users, active)
+
+console.log("users",userIsActive, "is Active");
+
+//Завдання п'яте 
+users.forEach(user => {
+    console.log(user.email);
+})
+
+//Завдання шосте
+const getUsersWithAge = (users, min, max) => {
+    return users.filter(user => user.age >= min && user.age <= max);
+};
+
+console.log(getUsersWithAge(users, 20, 30));
+//Завдання сьоме
+const calculateTotalBalance = users => {
+    return users.reduce((total, user) => total + user.balance, 0)
+};
+
+console.log(calculateTotalBalance(users));
+
+//Завдання десяте
+const getSortedUniqueSkills = users => {
+    return Array.from(new Set(users.flatMap(user => user.skills).sort()))
+};
+
+const skillsAll = getSortedUniqueSkills(users);
+console.log(skillsAll);
